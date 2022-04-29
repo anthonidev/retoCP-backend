43d6eaa174d48ec8dde8e73481c3b12e4e770687
Restyled.io <commits@restyled.io>
2022-04-29 05:50:53 +0000

    Restyled by autopep8

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ CORS_ORIGIN_WHITELIST = [
 
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
-    
+
     'https://reto-cp.vercel.app/'
 
 ]
@@ -112,9 +112,9 @@ CORS_ALLOWED_ORIGINS = [
 
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
-    
+
     'https://reto-cp.vercel.app/'
-    
+
 
 ]
 PASSWORD_HASHERS = [
