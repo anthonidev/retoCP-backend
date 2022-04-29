2d90519603008b512b6fcba0e14cd0d383743699
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2022-04-29 00:07:34 -0500

    retoCPbackend

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+    
+    path('', admin.site.urls),
+    
+]
